6ac78c531e8948a27979e0cced43aa2e55163bab
travelbeeee <sochun1518@naver.com>
2021-06-30 11:54:53 +0900

    게임진행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -25,6 +25,55 @@ public class Application {
         playGame(makeCarList(carNameList), turn);
     }
 
+    private static ArrayList<Car> makeCarList(String[] carNameList) {
+        ArrayList<Car> carList = new ArrayList<>();
+        for (String carName : carNameList) {
+            carList.add(new Car(carName));
+        }
+        return carList;
+    }
+
+    private static void playGame(ArrayList<Car> carList, String turn) {
+        System.out.println("실행 결과");
+        int t = Integer.parseInt(turn);
+        for(int i = 0; i < t; i++){
+            playTurn(carList);
+        }
+        printResult(carList, getMaxPosition(carList));
+    }
+
+    private static int getMaxPosition(ArrayList<Car> carList){
+        int maxScore = 0;
+        for (Car car : carList) {
+            maxScore = Math.max(maxScore, car.getPosition());
+        }
+        return maxScore;
+    }
+
+    private static void printResult(ArrayList<Car> carList, int maxScore) {
+        ArrayList<String> winners = new ArrayList<>();
+        for (Car car : carList) {
+            if (car.getPosition() == maxScore){
+                winners.add(car.getName());
+            }
+        }
+        System.out.print("최종 우승자: " );
+        for (int i = 0; i < winners.size(); i++){
+            if(i != 0)
+                System.out.print(", ");
+            System.out.print(winners.get(i));
+        }
+    }
+
+    private static void playTurn(ArrayList<Car> carList) {
+        for (Car c : carList) {
+            int goOrNot = RandomUtils.nextInt(0, 9);
+            if(goOrNot >= 4)
+                c.moveCar();
+            c.printPosition();
+        }
+        System.out.println();
+    }
 
     // 숫자로 이루어진 값
     private static boolean checkTurnInput(String turn) {
