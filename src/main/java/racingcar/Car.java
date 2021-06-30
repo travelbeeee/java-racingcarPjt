9810250e3a4bac93e990d8a3256dd35527537513
travelbeeee <sochun1518@naver.com>
2021-06-30 11:54:24 +0900

    getter추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -20,4 +20,12 @@ public class Car {
         }
         System.out.println();
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
